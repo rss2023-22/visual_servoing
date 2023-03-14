85efd878c6edeb020a255ae8eddd97183e48e807
brrich <brrich@mit.edu>
2023-03-14 19:48:43 +0000

    updating line following

--- scripts/computer_vision/color_segmentation.py
+++ scripts/computer_vision/color_segmentation.py
@@ -46,7 +46,7 @@ def cd_color_segmentation(img, template = None, line_following = 1.0, testing = 
 			lower_bound = np.array([5,180,190])
 			upper_bound = np.array([35,255,255])
 		else: # for line following orange tape
-			lower_bound = np.array([1,100,50])
+			lower_bound = np.array([1,100,50]) #np.array([1,100,50]) # upper_bound = np.array([35,255,255])
 			upper_bound = np.array([35,255,255])
 		return [lower_bound,upper_bound]
 
@@ -58,8 +58,8 @@ def cd_color_segmentation(img, template = None, line_following = 1.0, testing = 
 		line_following = False
 	if testing:
 		testing = True
-		viz_original_img = True
-		viz_masked_img = True
+		viz_original_img = False
+		viz_masked_img = False
 		viz_eroded = False
 		viz_dilated = False
 		viz_box = True
@@ -68,6 +68,7 @@ def cd_color_segmentation(img, template = None, line_following = 1.0, testing = 
 
 	# BEGIN CODE 
 	############
+	imgOrig = img
 
 	# step 0: limit range if line following
 	if line_following: # crop image
@@ -88,34 +89,50 @@ def cd_color_segmentation(img, template = None, line_following = 1.0, testing = 
 
 	# step 3: use erosion and dilution
 	kernel1 = np.ones((5,5), np.uint8)
-	image_erod = cv2.erode(imagemask,kernel1,iterations=2) # 1 # 2
+	image_erod = cv2.erode(imagemask,kernel1,iterations=1) # 1 # 2
 	if testing and viz_eroded:
 		image_print(image_erod)
-	image_dila = cv2.dilate(image_erod,kernel1,iterations=5) # 2 # 6
+	image_dila = cv2.dilate(image_erod,kernel1,iterations=2) # 2 # 5
 	if testing and viz_dilated:
 		image_print(image_dila)
 
-	# step 4: get contours
+	# step 4: get contours, if multiple, take contour closest to the center of img (for line following)
 	ret,thresh = cv2.threshold(image_dila,127,255,0)
 	if testing:
-		contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) # CHANGE BCK
+		contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) # opencvNew version
 	else:
-		_, contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) # CHANGE BCK
+		_, contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) # opencvOld version
 	if len(contours) == 0: # no box!
 		return ((0,0),(0,0))
 	
+	def getDist(coords1,coords2): # x coord only
+		return abs(coords1[0]-coords2[0])
+	
 	if line_following:
-		cnt = contours[0]
+		image_center = np.asarray(image_dila.shape) / 2
+		image_center = tuple(image_center.astype('int32'))
+		closest_contour = None 
+		min_dist = float('Inf')
+		for contour in contours:
+			M = cv2.moments(contour)
+			center_X = int(M["m10"] / M["m00"]); center_Y = int(M["m01"] / M["m00"])
+			distances_to_center = getDist((image_center[1],image_center[0]), (center_X,center_Y))
+			if distances_to_center < min_dist:
+				min_dist = distances_to_center; closest_contour = contour
+		cnt = closest_contour
 	else:
 		cnt = contours[-1]
 
 	# step 5: get bounding box
 	x,y,w,h = cv2.boundingRect(cnt)
-	bounding_box = ((x,y+lowBound),(x+w,y+h+lowBound))
+	if line_following:
+		bounding_box = ((x,y+lowBound),(x+w,y+h+lowBound))
+	else:
+		bounding_box = ((x,y),(x+w,y+h))
 
 	# step 6: display original img with bounding rectangle!
 	if testing:
-		img = cv2.rectangle(img,bounding_box[0],bounding_box[1],(0,255,0),2)
+		img = cv2.rectangle(imgOrig,bounding_box[0],bounding_box[1],(0,255,0),2)
 		if viz_box:
 			image_print(img)
 
